2e80e3d354d751bece05714246810d07b1cf253c
Ambiya12 <ambiyadms@gmail.com>
2024-12-06 15:10:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  return (
+    <>
+      <form>
+        <div>
+          <label> Mail :</label>
+          <input 
+            type="email" 
+            name="email" 
+            value={formData.email} 
+            onChange={handleChange} 
+          />
+        </div>
+        <div>
+          <label> Password :</label>
+          <input 
+            type="password" 
+            name="password" 
+            value={formData.password} 
+            onChange={handleChange} 
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
